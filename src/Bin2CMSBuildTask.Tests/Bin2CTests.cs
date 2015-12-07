39889cb6c1343b1333e99782a551b5a5f8d1de80
ItsVeryWindy <itsverywindy@users.noreply.github.com>
2015-12-07 15:47:00 +0000

    added additional tests

--- src/Bin2CMSBuildTask.Tests/Bin2CTests.cs
+++ src/Bin2CMSBuildTask.Tests/Bin2CTests.cs
@@ -14,6 +14,7 @@ namespace Bin2CMSBuildTask.Tests
 		{
 			if(File.Exists("output.h")) File.Delete("output.h");
 			if(File.Exists("output.c")) File.Delete("output.c");
+			if(File.Exists("output.c")) File.Delete("nonexistent.txt");
 		}
 
 		[Test]
@@ -35,6 +36,44 @@ namespace Bin2CMSBuildTask.Tests
 			Assert.That(File.ReadAllText("output.h"), Is.EqualTo(File.ReadAllText("output_expected.h")));
 			Assert.That(File.Exists("output.c"), Is.True);
 			Assert.That(File.ReadAllText("output.c"), Is.EqualTo(File.ReadAllText("output_expected.c")));
+		}
+
+		[Test]
+		public void ShouldFailIfAFileCouldNotBeFound()
+		{
+			var buildEngine = A.Fake<IBuildEngine>();
+
+			var unitUnderTest = new Bin2C
+			{
+				BuildEngine = buildEngine,
+				InputAssemblies = new ITaskItem[] {
+					new TaskItem("nonexistent.txt")	
+				},
+				OutputFile = new TaskItem("output")
+			};
+
+			Assert.That(unitUnderTest.Execute(), Is.False);
+			Assert.That(File.Exists("output.h"), Is.False);
+			Assert.That(File.Exists("output.c"), Is.False);
+		}
+
+		[Test]
+		public void ShouldFailIfAFileCouldNotBeAccessed()
+		{
+			using (var fs = new FileStream("output.h", FileMode.Create, FileAccess.ReadWrite))
+			{
+				var buildEngine = A.Fake<IBuildEngine>();
+
+				var unitUnderTest = new Bin2C {
+					BuildEngine = buildEngine,
+					InputAssemblies = new ITaskItem[] {
+						new TaskItem("nonexistent.txt")	
+					},
+					OutputFile = new TaskItem("output")
+				};
+
+				Assert.That(unitUnderTest.Execute(), Is.False);
+			}
 		}
 	}
 }
--- src/Bin2CMSBuildTask/Bin2C.cs
+++ src/Bin2CMSBuildTask/Bin2C.cs
@@ -3,6 +3,7 @@ using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Bin2CMSBuildTask
 {
@@ -18,50 +19,55 @@ namespace Bin2CMSBuildTask
 
 		public override bool Execute()
 		{
+			if(InputAssemblies.Any(e => !File.Exists(e.ToString())))
+			{
+				return false;
+			}
+
 			var hFileName = string.Format("{0}.h", OutputFile);
 			var cFileName = string.Format("{0}.c", OutputFile);
 
 			try
 			{
-			using(var hFile = new FileStream(hFileName, FileMode.Create, FileAccess.Write))
-			{
-				using (var hsw = new StreamWriter(hFile))
+				using(var hFile = new FileStream(hFileName, FileMode.Create, FileAccess.Write))
 				{
-					var outputFileTitle = _titleFormat.Replace(OutputFile.ToString(), "_").ToUpper();
-
-					hsw.WriteLine("#ifndef __{0}_H__", outputFileTitle);
-					hsw.WriteLine("#define __{0}_H__", outputFileTitle);
-
-					using (var cFile = new FileStream(cFileName, FileMode.Create, FileAccess.Write))
+					using (var hsw = new StreamWriter(hFile))
 					{
-						using (var csw = new StreamWriter(cFile))
+						var outputFileTitle = _titleFormat.Replace(OutputFile.ToString(), "_").ToUpper();
+
+						hsw.WriteLine("#ifndef __{0}_H__", outputFileTitle);
+						hsw.WriteLine("#define __{0}_H__", outputFileTitle);
+
+						using (var cFile = new FileStream(cFileName, FileMode.Create, FileAccess.Write))
 						{
-							foreach(var item in InputAssemblies)
+							using (var csw = new StreamWriter(cFile))
 							{
-								var title = _titleFormat.Replace(item.ToString(), "_");
-
-								hsw.WriteLine("extern const unsigned char * {0};", title);
-
-								csw.Write("const unsigned char * {0} = \"", title);
-
-								using(var file = new FileStream(item.ToString(), FileMode.Open, FileAccess.Read))
+								foreach(var item in InputAssemblies)
 								{
-									int b;
+									var title = _titleFormat.Replace(item.ToString(), "_");
 
-									while((b = file.ReadByte()) >= 0)
+									hsw.WriteLine("extern const unsigned char * {0};", title);
+
+									csw.Write("const unsigned char * {0} = \"", title);
+
+									using(var file = new FileStream(item.ToString(), FileMode.Open, FileAccess.Read))
 									{
-										csw.Write("\\x{0:X2}", b);
-									}
-								}
+										int b;
 
-								csw.WriteLine("\";");
+										while((b = file.ReadByte()) >= 0)
+										{
+											csw.Write("\\x{0:X2}", b);
+										}
+									}
+
+									csw.WriteLine("\";");
+								}
 							}
 						}
-					}
 
-					hsw.WriteLine("#endif");
+						hsw.WriteLine("#endif");
+					}
 				}
-			}
 			}
 			catch(IOException)
 			{
