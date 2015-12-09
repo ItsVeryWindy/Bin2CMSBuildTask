17d95a4948de111e5333e5c18ad0ea5d9f1ee921
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2015-12-09 10:11:45 +0000

    fixing packaging

--- src/Bin2CMSBuildTask/Properties/AssemblyInfo.cs
+++ src/Bin2CMSBuildTask/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("oliverneal")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
